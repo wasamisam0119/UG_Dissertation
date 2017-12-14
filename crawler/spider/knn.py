ab7980bf040ee6750b479bc48e506c7632aa092b
wasamisam0119 <zhaoshujia0123@gmail.com>
2017-12-14 15:35:26 +0000

    improve robust

--- crawler/spider/knn.py
+++ crawler/spider/knn.py
@@ -1,19 +1,24 @@
 import region_extractor 
 import json_reader 
 import math
-attribute = ["num_bath","num_bed","price","month_view","coordinate","property_type"]
-def house_KNN(house_info,neighbours,K):
+from statistics import median
+
+homo_attribute = ["num_bath","num_bed","price","month_view","coordinate","property_type"]
+hetero_attribute = ["num_bath","num_bed","month_view","coordinate","property_type"]
+
+def house_KNN(house_info,neighbours,K,attribute_list):
     neighbour_list = []
     for neighbour in neighbours:
         #print(neighbour)
-        neighbour_list.append((calculate_weightedDistance(house_info,neighbour),neighbour))
+        neighbour_list.append((calculate_weightedDistance(house_info,neighbour,attribute_list),neighbour))
     K_neighbours_list = sorted(neighbour_list,key = lambda t:t[0],reverse = False)[:K]
     print(K_neighbours_list)
+    return K_neighbours_list
 
-def calculate_weightedDistance(house_info,neighbour):
+def calculate_weightedDistance(house_info,neighbour,attribute):
     distance_list = []
     distance = 0
-    for i in range(4):
+    for i in range(len(attribute)-2):
         distance_list.append(math.sqrt((house_info[attribute[i]]-neighbour[attribute[i]])**2))
     geo_distance = math.sqrt((float(house_info["coordinate"][0])-float(neighbour["coordinate"][0]))**2 + (float(house_info["coordinate"][1])-float(neighbour["coordinate"][1]))**2)
     distance_list.append(geo_distance)
@@ -24,8 +29,6 @@ def calculate_weightedDistance(house_info,neighbour):
         distance_list.append(1)
     for feature_distance in distance_list:
         distance += feature_distance*gaussian_kernel(feature_distance)
-    print(distance_list)
-
     return distance
 
 def gaussian_kernel(feature_distance):
@@ -64,11 +67,22 @@ def extract_house_info(house_id):
         else:
             return 0
 
-#region_houses_dict = locate_range("43768750","sale_region/","sale")
-#house_info = extract_house_info("43768750")
-#house_KNN(house_info,region_houses_dict,10)
+def calculate_mid_price(neighbours):
+    price_list = []
+    for house in neighbours:
+        price_list.append(house[1]["price"])
+    return median(price_list)
 
 
-        
-    
-region_houses_dict = locate_range("43768750","sale_region/","sale")
+def estimate_price(houseid,sale_type,estimate_type):
+    region_houses_dict = locate_range(houseid,estimate_type+"_region/",sale_type)
+    house_info = extract_house_info(houseid)
+    if sale_type==estimate_type:
+        neighbours = house_KNN(house_info,region_houses_dict,10,homo_attribute)
+    else:
+        neighbours = house_KNN(house_info,region_houses_dict,10,hetero_attribute)
+    return neighbours
+
+
+neighbours = estimate_price("45101554","sale","rent")
+print(calculate_mid_price(neighbours))
