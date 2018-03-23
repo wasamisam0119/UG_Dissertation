b714bcf2b8b641afff71b0816cf65c587de60bba
wasamisam0119 <zhaoshujia0123@gmail.com>
2018-03-23 22:30:56 +0000

    predefine var

---
+++ crawler/spider/pre_define.py
@@ -0,0 +1,3 @@
+import pandas as pd
+train_features = ["num_bath","num_bed","month_view","coordinate"]
+weights = pd.DataFrame([  2.15520417*10**-2,   1.19257808*10**-2,   3.14956906*10**-7,   7.93325839*10**-6]).T
