00869e72af67959da7e847373f9f5fc10ded4a1b
wasamisam0119 <zhaoshujia0123@gmail.com>
2018-03-29 01:48:36 +0100

    add column city

--- crawler/spider/AreaPipeline.py
+++ crawler/spider/AreaPipeline.py
@@ -1,14 +1,16 @@
 import pandas as pd
 from DataFetcher import DataFetcher 
 from sqlalchemy import create_engine  
-
+import re
 dropping_feature = ['crawling_time','top3near_by']
 groupby_feature = ["region","property_type"]
 merge_key = ["region","property_type"]
+citypattern = re.compile("[A-Za-z]+")
 
 class AreaPipeline(object):
     def __init__(self,area_config):
         self.read_config(area_config)
+        self.city = pd.read_csv("postcode.csv")
         
         self.s = DataFetcher("sale",self.pattern)
         self.r = DataFetcher("rent",self.pattern)
@@ -36,18 +38,21 @@ class AreaPipeline(object):
         area_r_df.columns = groupby_feature+["rent_speed","r_monthview","rent_price"]
         area_df = pd.merge(area_s_df,area_r_df,on = merge_key)
         area_df['price-to-rent'] = (area_df['rent_price']/area_df['sale_price']).round(5)
+
+        area_df['city'] = area_df.apply(self.matchcity,axis = 1)
         return area_df
 
+    def matchcity(self,row):
+        region = citypattern.match(row.region).group(0)
+        return self.city.values[self.city['postcode'] ==region][0][1]
 
 
-                
-                
 
 
 
-#t = AreaPipeline("area_config.txt")
-#area_df = t.area_process()
+t = AreaPipeline("area_config.txt")
+area_df = t.area_process()
 #%run area_info_retri.py
 
 #engine =create_engine('mysql+pymysql://root:@localhost:3306/G53DT?charset=utf8')
-#area_df.to_sql("Area",engine,if_exists = 'replace')
+#area_df.to_sql("SuperArea",engine,if_exists = 'replace')
